1db627952ce9aa7491287aa3d862ae75e8401199
Sidhartha-developer <ssidhartha209@gmail.com>
2026-05-28 12:10:54 +0530

    updated again

--- src/config/mailer.js
+++ src/config/mailer.js
@@ -1,9 +1,16 @@
 const nodemailer = require('nodemailer');
+
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: Number(process.env.MAIL_PORT) || 587,
-  secure: false,
-  auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
+  service: 'gmail',
+
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 transporter.verify((err, success) => {
@@ -13,4 +20,5 @@ transporter.verify((err, success) => {
     console.log('MAIL SERVER READY');
   }
 });
+
 module.exports = transporter;
